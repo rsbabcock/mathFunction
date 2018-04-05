740c69adbffef083965b871d7235d7b616494083
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-05 09:33:20 -0500

    Added notes to read me and a calculate function

---
+++ README.md
@@ -0,0 +1,5 @@
+# uses very conncise arrow functions
+
+# also calculate is an intro to asynchonous programming, also known as a Lambda
+
+# Lambda = Passing a function to another function
--- main.js
+++ main.js
@@ -3,8 +3,18 @@
 // 2. Create another function that logs the result of subtracting two numbers that were passed in as arguments.
 // 3. Invoke each function a couple times with different argument values.
 
-function math (num1, num2) => {
-    return num1 + num2;
+const math = (num1, num2) => console.log(num1+num2)
+
+math(1,4)
+
+const subtract = (num1, num2) => console.log(num1 - num2)
+
+subtract(5,2)
+
+// Create another function called `calculate` that takes three arguments: two numbers and a function
+
+const calculate = (num1, num2, operation) => {
+    operation(num1, num2);
 }
 
-console.log(math(1,3));
+calculate(100,99, subtract);
