f775a94d52fc0eb27c9f0ba8f1eae40d3060ccf2
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-05 09:10:52 -0500

    created two functions that add and subtract, one does each function

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// 1. Create a function that logs the result of adding two numbers that were passed in as arguments.
+// 2. Create another function that logs the result of subtracting two numbers that were passed in as arguments.
+// 3. Invoke each function a couple times with different argument values.
+
+function math (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(math(1,3));
